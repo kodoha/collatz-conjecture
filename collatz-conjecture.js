da76398546ede30234352d89c888735bb843f40d
kodoha <86849478+kodoha@users.noreply.github.com>
2021-07-08 12:17:25 -0700

    Update collatz-conjecture.js

--- collatz-conjecture.js
+++ collatz-conjecture.js
@@ -1,7 +1,4 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
 
 export const steps = (n) => {
 if( n <= 0) {
